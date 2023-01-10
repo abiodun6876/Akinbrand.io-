4bb4f805e5ed963fc95944b9a8d3d3cceaa402f5
AKINPELU Abiodun Moses <70350694+abiodun6876@users.noreply.github.com>
2023-01-10 16:56:11 +0100

    Create Brain.js

---
+++ Brain.js
@@ -0,0 +1,256 @@
+// List of likely questions and answers
+
+const questions = [
+
+{
+
+keywords: ['activate', 'Activate'],
+
+answer: 'To activate your Glo SIM card, follow these steps:\n1. Insert the SIM card into your phone.\n2.dial the code *125# using the already registered Glo SIM card\n3. and recharge airtime of at least N100.\n4. Glo SIM activation is a quick process that allows your new SIM to make or receive calls \n5.and perform other services on the Glo network.'
+
+},
+
+{
+
+keywords: ['day','Good', 'morning','aftoon','evening','Day','Aftoon','Morming','Evening','good'],
+
+answer: 'hi! how can i help you?'
+
+},
+
+{
+
+keywords: ['check', 'data', 'balance'],
+
+answer: 'To check your data balance, follow these steps:\n1. Dial #124# from your phone.\n2. Press the call button.\n3. Follow the on-screen prompts to view your data balance.'
+
+},
+
+{
+
+keywords: ['top', 'up'],
+
+answer: 'To top up your account, you can do one of the following:\n1. Buy your a glo recharge card (N100 and above)\n2.Scratch it gently if it as a silver panel.\n3.Press *323*PIN# for example *323* 555558888853245#\n4Press Ok/send on your phone.\n5. check your account balance.'
+
+},
+
+{
+
+keywords: ['unlock', 'phone'],
+
+answer: 'To unlock your phone, you will need to contact your phone carrier or manufacturer. They will be able to provide you with the necessary information and steps to unlock your phone.'
+
+},
+
+{
+
+keywords: ['change', 'plan'],
+
+answer: 'Simply dial *311# and you are migrated to Glo 11k per sec plan.\n1. where you enjoy 11k/s to all networks in Nigeria.'
+
+},
+
+{
+
+keywords: ['pay', 'bill'],
+
+answer: 'To pay your phone bill, you can do one of the following:\n1. Pay online using a credit or debit card.\n2. Pay in person at a retail location.\n3. Pay by phone by calling your phone carrier and providing your payment information.\n4. Set up automatic payments to have your bill paid automatically each month.'
+
+},
+
+{
+
+keywords: ['sign', 'contract'],
+
+answer: 'To sign a phone contract, you will need to visit a retail location or contact your phone carrier directly. You will be required to provide personal and financial information to qualify for the contract. You will also need to agree to the terms and conditions of the contract. Once the contract is signed, you will be responsible for paying the monthly bill and any additional fees or charges as outlined in the contract.'
+
+},
+
+{
+
+keywords: ['cancel', 'contract'],
+
+answer: 'To cancel a phone contract, you will need to contact your phone carrier directly. They will be able to provide you with the necessary information and steps to cancel your contract. Please note that there may be termination fees or other charges associated with canceling a contract early.'
+
+},
+
+{
+
+keywords: ['transfer', 'credit'],
+
+answer: 'To transfer credit from one phone to another, Dial *131# and wait for the prompt.\n1. If successful, Glo would request that you input the recipient phone number as well as the amount you intend to transfer and your pin too.\n2. If you would rather dial everything at a go,\n3. simply use the code: *131*recipients number*amount* pin#'
+
+},
+
+{
+
+keywords: ['activate', 'roaming'],
+
+answer: 'To activate roaming on your phone, follow these steps:\n1. Contact your phone carrier to ensure that your phone plan includes roaming.\n2. Enable roaming on your phone by going to the "Settings" menu and selecting "Cellular" or "Mobile Data".\n3. Toggle the "Roaming" or "Data Roaming" option to "On".\n4. Your phone should now be able to use roaming services while you are traveling abroad.'
+
+},
+
+{
+
+keywords: ['check', 'balance'],
+
+answer: 'To check your phone balance, you can do one of the following:\n1. Dial the balance check code for your phone carrier (e.g. *123#).\n2. Check your balance online by logging into your account on your phone carrier website.\n3. Check your balance through your phone carrier mobile app.\n4. Contact your phone carrier and ask them to provide you with your current balance.'
+
+},
+
+{
+
+keywords: ['add', 'data'],
+
+answer: 'To add data to your phone plan, follow these steps:\n1. Determine how much data you need by considering your usage habits (e.g. streaming video, browsing the web, etc.).\n2. Contact your phone carrier to discuss available data plans and pricing.\n3. Select the data plan that best fits your needs.\n4. Follow the instructions provided by your phone carrier to activate the new data plan. This may involve paying an additional fee.\n5. Your phone should now have the additional data added to it.'
+
+},
+
+{
+
+keywords: ['Thanks', 'THANK','thanks','you','You'],
+
+answer: 'You are welcome. Am glad to be of help,thanks for using Glo services.'
+
+},
+
+{
+
+keywords: ['troubleshoot', 'no', 'service'],
+
+answer: 'If you are experiencing issues with your phone service, try the following steps:\n1. Restart your phone.\n2. Check to make sure that your phone is not in "Airplane Mode".\n3. Verify that your phone is compatible with the network frequencies used by your phone carrier.\n4. Check for any network outages or maintenance updates by contacting your phone carrier.\n5. Try switching to a different network mode (e.g. 3G, 4G, etc.).\n6. If the issue persists, contact your phone carrier for further assistance.'
+
+},
+
+{
+
+keywords: ['hi', 'hello', 'hey', 'Hello','Hi','Hey','HELLO','HI','HEY'],
+
+answer: 'hi How may I help you?'
+
+},
+
+{
+
+keywords: ['glo', 'migration', 'code', 'yakata'],
+
+answer: 'For old customers on other tariff plans dial *455# to move to Yakata and recharge to enjoy Yakata benefits on every recharge.'
+
+},
+
+{
+
+keywords: [ 'GLO','BEREKETE', '10X', 'TARIFF', 'PLAN?'],
+
+answer: 'N1,000 Joining Bonus for all New Customers, which can be used for ALL-NET calls and data. The bonus is given one-off after activating a new Glo SIM with minimum of N100 recharge and making your first call.Whopping 10X Bonus (400% for Voice & 500% for Data) on all recharges, this can be used for ALL-NET calls and data.Special Extra Data Bonus on first recharge of the month (N100 and above) for 6 months from the date of joining Glo Network.Up to 100% Bonus on Data Plan Purchases for 4 months.BEREKETE 10X replaces BEREKETE as the default tariff plan for new customers. Already existing Glo customers can also enjoy BEREKETE 10X benefits when they dial *777# (please note that existing customers are not eligible for the one-off welcome bonus).'
+
+},
+
+{
+
+keywords: ['Glo','airtime','account','balance','code'],
+
+answer: 'Dial #124# on your mobile device then wait for an instant reply from Glo revealing your airtime balance on your phone screen.'
+
+},
+
+{
+
+keywords: ['Glo','bonus', 'data', 'balance', 'code'],
+
+answer: 'Dial #124# > Enter 1 on your mobile device then wait for an instant reply from Glo revealing your bonus data balance on your phone screen.'
+
+},
+
+{
+
+keywords: ['Glo','Simple', 'Recharge'],
+
+answer: 'For glo simple recharge,Dial *805# on your phone'
+
+},
+
+{
+
+keywords: ['Glo', 'data', 'gifting'],
+
+answer: 'All Glo USSD Code Function*805#.Glo Simple Recharge*127*[USSD_Plan_Number]*[friend’s number]#'
+
+},
+
+{
+
+keywords: ['Glo', 'data', 'sharing'],
+
+answer: 'For your Glo data sharing. Dail,*127*01*[friend’s number]# on your mobile phone'
+
+},
+
+{
+
+keywords: ['Check', 'Glo number'],
+
+answer: 'kindly dial *135*8# on your phone'
+
+},
+
+{
+
+keywords: ['Subscribe', 'Glo', 'YAKATA'],
+
+answer: 'To Subscribe to Glo YAKATA, dial *230#'
+
+},
+
+{
+
+keywords: ['Check','Glo', 'data', 'balance'],
+
+answer: 'To Check Glo data balance,simply dia *777# on your mobile phone'
+
+},
+
+{
+
+keywords: ['Borrow', 'data', 'credit'],
+
+answer: 'To borrow data or credit on your glo SIM. Dial *321#'
+
+},
+
+{
+
+keywords: ['Buy', 'Glo', 'data'],
+
+answer: 'To buy Glo mobile data, simply dia:*777#'
+
+},
+
+{
+
+keywords: ['Migrate','Friends', 'Family', 'Glo', 'list'],
+
+answer: 'To Migrate to Friends & Family Glo list. Dial :*100*9*2# on your mobile phone'
+
+},
+
+{
+
+keywords: ['frequently', 'called', 'Family', 'Friends','numbers'],
+
+answer: 'You can Add 10 frequently called Family and Friends numbers. dial:*101*1*Mobile No#'
+
+},
+
+{
+
+keywords: ['Check', 'Glo','data', 'balance'],
+
+answer: 'To Check Glo data balance, kindly dail:*127*0#'
+
+}
+
+];
+
+ 
+
