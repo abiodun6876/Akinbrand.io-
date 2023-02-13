7c531739479105b705f93641e2360591126c0670
AKINPELU Abiodun Moses <70350694+abiodun6876@users.noreply.github.com>
2023-02-14 00:30:13 +0100

    Create Calculator.cpp
    How to create basic numeric operation calculator with c++

---
+++ Calculator.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  double num1, num2;
+
+  char operation;
+
+  cout << "Enter first number: ";
+
+  cin >> num1;
+
+  cout << "Enter second number: ";
+
+  cin >> num2;
+
+  cout << "Enter operator (+, -, *, /): ";
+
+  cin >> operation;
+
+  switch (operation) {
+
+    case '+':
+
+      cout << num1 + num2;
+
+      break;
+
+    case '-':
+
+      cout << num1 - num2;
+
+      break;
+
+    case '*':
+
+      cout << num1 * num2;
+
+      break;
+
+    case '/':
+
+      cout << num1 / num2;
+
+      break;
+
+    default:
+
+      cout << "Invalid operator";
+
+  }
+
+  return 0;
+
+}
+
